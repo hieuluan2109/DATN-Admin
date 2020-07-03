7ee4fb73a6ba96403c2eb775c174a4aedf087274
LuanNguyen <kesanbauvat9@gmail.com>
2020-07-03 21:18:40 +0700

    [BA][Backend]Admin login
    Fix port in setHeader

--- Backend/app.js
+++ Backend/app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+
 const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const ExtractJwt = passportJWT.ExtractJwt;
+const JWTStrategy = passportJWT.Strategy;
+let jwtoptions = {};
+const jwt = require('jsonwebtoken');
+jwtoptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtoptions.secretOrKey = process.env.SESSION_SECRET;
+
 const mongoose = require('mongoose');
 const session = require('express-session');
 const port = process.env.PORT || 8000;
 const flash = require('connect-flash');
 require('./config/passport')(passport);
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
+// app.set('views', './views');
+// app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use( session({
@@ -30,22 +39,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
     console.log('Kết nối thành công !!');
 });
-app.use( function(req, res, next){ // fix lỗi # domain, npm cors
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-})
+app.use( require('./config/setHeader') );
 app.use('/admin', require('./routers/admin_router'));
 
 app.listen( port , (error) => {
--- Backend/model/adminSchema.js
+++ Backend/model/adminSchema.js
@@ -2,16 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const AdminSchema = new Schema({
     ho: {
+        required: true,
         type: String
     },
     ten: {
+        required: true,
         type: String
     },
     anh_dai_dien: String,
-    ngay_sinh: Date,
-    mat_khau: String,
-    loai: Boolean
-});
+    ngay_sinh: {
+        required: true,
+        type: String
+    },
+    mat_khau: {
+        required: true,
+        type: String
+    },
+    loai: {
+        required: true,
+        type: String
+    },
 
 // AdminSchema
 //     .virtual('ho_ten')

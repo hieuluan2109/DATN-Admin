0af2dab3688e03ee662b281701eca02866d2a1de
VoVanKiet <50176661+vankiet735@users.noreply.github.com>
2020-07-05 03:36:50 +0700

    Popup Forget password

--- Frontend/src/component/Login/Login_Form.js
+++ Frontend/src/component/Login/Login_Form.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 // import Status from './StatusRequest'
-
 import axios from 'axios'
+import Popup from "reactjs-popup";
+import ForgotPassword from './ForgotPassword';
 class LoginForm extends Component {
     constructor(props){
         super(props)
@@ -22,21 +23,29 @@ class LoginForm extends Component {
     //   }
             handleSubmit=event=>{
              event.preventDefault();
-            // const{email,password}=this.state
+            const{email,password}=this.state
             // POST #1
-            // axios({
-            //     method: 'post',
-            //     url: 'http://localhost:8000/admin/login',
-            //     data: {email,password}
-            //   }).then(res=>{
-            //         console.log(res)
-            //   }).
-            //   catch((error) => console.log( error.response.request._response ) );
+            axios({
+                method: 'post',
+                url: 'http://localhost:8000/admin/login',
+                data: {email,password}
+              }).then(res=>{
+                    console.log(res.data)
+              }).catch((error) => {
+                    console.log("Lỗi",error.response.data )  
+                        if(error.response.data.success===false){
+                            
+            }});
+             
             //   console.log({email,password})
 
              // POST #2
-            //  axios.post('http://localhost:8000/admin/login',{email:this.state.email,password:this.state.password}).then(res=>{
-            //     console.log(res);
+            //  axios.get('http://localhost:8000/admin/logout').then(res=>{
+            //     const persons = res.data;
+            //        this.setState({ persons });
+            //       console.log(this.state.persons)
+            // //     this.setState({ persons });
+            // //     console.log(this.state.persons)
             //     // if(res.msg=="true")return <Redirect to='/a' />
             //     // else return <Redirect to='/b' />
             //  }).catch(error => console.log("Lỗi" ,error));
@@ -44,13 +53,13 @@ class LoginForm extends Component {
             //  console.log(this.state.password);
 
                 //GET dữ liệu được
-            axios.get('http://localhost:8000/admin/profile').then(res=>{
-                const persons = res.data;
-                this.setState({ persons });
-                console.log(this.state.persons)
-             }).catch(error => console.log("Lỗi" ,error));
-             console.log(this.state.email);
-             console.log(this.state.password);
+            // axios.get('http://localhost:8000/admin/profile').then(res=>{
+            //     const persons = res.data;
+            //     this.setState({ persons });
+            //     console.log(this.state.persons)
+            //  }).catch(error => console.log("Lỗi" ,error));
+            //  console.log(this.state.email);
+            //  console.log(this.state.password);
     }
     render() {
         return (
@@ -58,13 +67,18 @@ class LoginForm extends Component {
                 <form onSubmit={this.handleSubmit}>
                     <h2> Login </h2>
                     <div><i id="user" className="fas fa-user"></i> </div>
-                    <div> <input type="email" placeholder="Email" name="email" value={this.state.name} onChange={this.handleChange}></input></div>
+                    <div> <input className="iput" type="email" placeholder="Email" name="email" value={this.state.name} onChange={this.handleChange}></input></div>
                     <div><i id="lock" className="fa fa-lock" ></i></div>
-                    <div><input type="password" placeholder="********" name="password" value={this.state.password} onChange={this.handleChange} ></input></div>
-                    <div> <input type="checkbox"></input><label id="remember">Remember me</label> </div>
+                    <div><input className="iput" type="password" placeholder="********" name="password" value={this.state.password} onChange={this.handleChange} ></input></div>
+                    <div className="checkb"> <input type="checkbox"></input><label id="remember">Remember me</label> </div>
                     <div><input type="submit" className="btn" value="Login" /> </div>
-                    <a href="/">Forget Password? </a>
+                    
                 </form>
+           
+                    <Popup  trigger={<span>Forget Password</span>} modal> 
+                         {close => <ForgotPassword close={close} />}
+                    </Popup>
+                   
             </div>
         );
     }

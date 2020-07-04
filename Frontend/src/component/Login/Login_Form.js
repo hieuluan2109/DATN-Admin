0da7c8cedf4918111ee1cf116d121b5c89255996
VoVanKiet <50176661+vankiet735@users.noreply.github.com>
2020-07-04 15:20:05 +0700

    !!
    Admin Login #2  (Còn BUG)

--- Frontend/src/component/Login/Login_Form.js
+++ Frontend/src/component/Login/Login_Form.js
@@ -1,14 +1,66 @@
 import React, { Component } from 'react'
+// import Status from './StatusRequest'
+
+import axios from 'axios'
 class LoginForm extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            email:"",
+            password:""
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value           
+        });
+      }
+    //   handleChanges = (event) => {
+    //     this.setState({ password: event.target.value           
+    //     });
+    //   }
+            handleSubmit=event=>{
+             event.preventDefault();
+            // const{email,password}=this.state
+            // POST #1
+            // axios({
+            //     method: 'post',
+            //     url: 'http://localhost:8000/admin/login',
+            //     data: {email,password}
+            //   }).then(res=>{
+            //         console.log(res)
+            //   }).
+            //   catch((error) => console.log( error.response.request._response ) );
+            //   console.log({email,password})
+
+             // POST #2
+            //  axios.post('http://localhost:8000/admin/login',{email:this.state.email,password:this.state.password}).then(res=>{
+            //     console.log(res);
+            //     // if(res.msg=="true")return <Redirect to='/a' />
+            //     // else return <Redirect to='/b' />
+            //  }).catch(error => console.log("Lỗi" ,error));
+            //  console.log(this.state.email);
+            //  console.log(this.state.password);
+
+                //GET dữ liệu được
+            axios.get('http://localhost:8000/admin/profile').then(res=>{
+                const persons = res.data;
+                this.setState({ persons });
+                console.log(this.state.persons)
+             }).catch(error => console.log("Lỗi" ,error));
+             console.log(this.state.email);
+             console.log(this.state.password);
+    }
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <h2> Login </h2>
                     <div><i id="user" className="fas fa-user"></i> </div>
-                    <div> <input type="email" placeholder="Email" id="Email"></input></div>
+                    <div> <input type="email" placeholder="Email" name="email" value={this.state.name} onChange={this.handleChange}></input></div>
                     <div><i id="lock" className="fa fa-lock" ></i></div>
-                    <div><input type="password" placeholder="********" id="password" ></input></div>
+                    <div><input type="password" placeholder="********" name="password" value={this.state.password} onChange={this.handleChange} ></input></div>
                     <div> <input type="checkbox"></input><label id="remember">Remember me</label> </div>
                     <div><input type="submit" className="btn" value="Login" /> </div>
                     <a href="/">Forget Password? </a>
